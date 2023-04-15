66c14ae7f04b37084621f41cb0bd2da5ddb5a4a6
klevita <kklevita92@gmail.com>
2023-04-15 15:05:46 +0300

    deploy fixes

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  publicPath: '/Vita-Disk/',
+  publicPath: '/',
   transpileDependencies: [
     'vuetify'
   ],
